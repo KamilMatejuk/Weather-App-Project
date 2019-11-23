a0992fa0db83fe9af19f81341eb8fe2a1e5e6ce3
Marek Sołdaczuk <forexampleee@wp.pl>
2019-11-23 23:18:25 +0100

    pobeiranie danych z API

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,14 @@ function weather( cityName ) {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
-      console.log(data);
+      console.log(data); // pelne info
+      //console.log(data.name);
+     // console.log(data.weather[0].description);
+      //console.log(data.main.temp);
+      //console.log(Math.round(parseFloat(data.main.temp)-273.15) + '°C');
+      // fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+      
+     // displayWeather(data);   //przykladowe wywolanie wyswietlania dla danych
     })
     .catch(function() {
       // catch any errors
