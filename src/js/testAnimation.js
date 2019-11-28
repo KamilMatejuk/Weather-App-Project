681d9738ecd4157841c532f107cebfec017e5b70
Teresa Kozera <tekozera@gmail.com>
2019-11-28 21:35:21 +0100

    Event listeners for details button and display mode buttons added

--- src/js/testAnimation.js
+++ src/js/testAnimation.js
@@ -15,3 +15,25 @@ window.onload = function () {
     document.body.scrollTop = document.documentElement.scrollTop = 0;
     window.onscroll = function () {};
 }
+
+let toggleButtonDetails = false;
+let buttonDetails = document.getElementById("btn-detail").addEventListener('click', () => {
+    $(document.getElementById("details")).toggleClass('none', toggleButtonDetails);
+    toggleButtonDetails = !toggleButtonDetails;
+});
+
+let buttonDisplayMode = document.querySelector(".weather-icon").addEventListener('click', (e) => {
+    const icon = ".weather-icon";
+    if(e.target.classList.contains("icon-sun")) {
+        $( icon ).toggleClass( "icon-sun", false );
+        // for now, later should be changed to an icon with a moon or something like this
+        $( icon ).toggleClass( "icon-cloud", true );
+        document.querySelector(".cover").style.backgroundImage = "url('../img/bg_sunny.jpg')";
+    } else {
+        $( icon ).toggleClass( "icon-cloud", false );
+        // for now, later should be changed to an icon with a moon or something like this
+        $( icon ).toggleClass( "icon-sun", true );
+        document.querySelector(".cover").style.backgroundImage = "url('../img/Night-sky.jpg')";
+    }
+});
+
