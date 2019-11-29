cd13826c39110072a0fb835f4714a967a2c19605
Kamil Matejuk <kamilmatejuk@gmail.com>
2019-11-29 01:13:29 +0100

    Updated christmas theme

--- src/js/christmas.js
+++ src/js/christmas.js
@@ -3,17 +3,24 @@ const data = new Date();
 if(data.getMonth() == 11){
     // tydzień przed świetami i do końca świąt, oraz na mikołajki
     if((data.getDate() >= 17 && data.getDate() <= 26) || data.getDate() == 6){
-        const addition = document.createElement('div');
-        addition.classList.add('time');
-        addition.style.width = '150px';
-        addition.style.height = '150px';
-        addition.style.backgroundImage = 'url("../img/christmas_hat.png")';
-        addition.style.backgroundSize = 'contain';
-        addition.style.position = 'absolute';
-        addition.style.top = '-77px';
-        addition.style.right = '-55px';
-
-        const basic = document.getElementById('basic-data');
-        basic.appendChild(addition);
+        add('url("../img/christmas_hat.png")');
     }
+}
+
+// do testów
+// add('url("../img/christmas_hat.png")');
+
+function add(url){
+    const addition = document.createElement('div');
+    addition.classList.add('time');
+    addition.style.width = '150px';
+    addition.style.height = '150px';
+    addition.style.backgroundImage = url;
+    addition.style.backgroundSize = 'contain';
+    addition.style.position = 'absolute';
+    addition.style.top = '-77px';
+    addition.style.right = '-55px';
+
+    const basic = document.getElementById('basic-data');
+    basic.appendChild(addition);
 }
